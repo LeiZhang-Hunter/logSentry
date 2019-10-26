3c084dc80ac88fe182ffa22029c761908e7f4c15
zhanglei <zhanglei1@chelun.com>
2019-10-26 19:50:06 +0800

    协议问题修改

--- src/app/ProtoBufMsg.cpp
+++ src/app/ProtoBufMsg.cpp
@@ -16,10 +16,10 @@ size_t* ProtoBufMsg::encodeProtoStruct(const char* json_buffer) {
     size_t body_len;//包体的长度
 
     //包头的长度
-    header_len = sizeof(header_len);
+    header_len =sizeof(protoHeader);
     //计算消息体的长度
     body_len = strlen(json_buffer)+1;
-    protoLen = sizeof(size_t)+sizeof(protoHeader)+body_len;
+    protoLen = sizeof(size_t)+header_len+body_len;
 
     //申请内存地址
     auto proto_addr = (size_t *)malloc(protoLen);
@@ -31,16 +31,16 @@ size_t* ProtoBufMsg::encodeProtoStruct(const char* json_buffer) {
 
     *proto_addr = os->htonll(protoLen);
     proto_addr++;
-    header.version = SENTRY_VERSION;
-    header.server = 0;
-    header.magic = MAGIC;
-    header.body_len = os->htonll(body_len);
+
 
     auto dataStruct = (protoHeader*)proto_addr;
-    //包头的长度
-    memcpy(dataStruct,&header,header_len);
-    dataStruct++;
+    dataStruct->version = SENTRY_VERSION;
+    dataStruct->server = 0;
+    dataStruct->magic = MAGIC;
+    dataStruct->body_len = os->htonll(body_len);
 
+    //包头的长度
+    dataStruct++;
     memcpy(dataStruct,json_buffer,body_len);
     return save_ptr;
 }
