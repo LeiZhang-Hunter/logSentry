e271cedaa62df258b3bebacefba383cf0728f285
zhanglei <zhanglei1@chelun.com>
2019-11-12 15:20:22 +0800

    删除掉没用的内容

--- NodeSentryWeb/Public/js/common/extensionObject.js
+++
@@ -1,9 +0,0 @@
-var extensionObject = {
-    download:function(dom,downloadUrl){
-
-        //dom 元素点击事件
-        dom.click(function () {
-            window.location.href = downloadUrl;
-        });
-    }
-};
