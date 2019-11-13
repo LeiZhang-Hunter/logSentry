5678c93f4106ea4b61d0d864a86272da5689a075
zhanglei <zhanglei1@chelun.com>
2019-11-13 18:21:55 +0800

    去掉无用的printf

--- NodeSentryClient/src/app/ProtoBufMsg.cpp
+++ NodeSentryClient/src/app/ProtoBufMsg.cpp
@@ -19,7 +19,6 @@ size_t* ProtoBufMsg::encodeProtoStruct(const char* json_buffer) {
     //计算消息体的长度
     body_len = strlen(json_buffer)+1;
     protoLen = sizeof(size_t)+header_len+body_len;
-    printf("ptoto len:%ld\n",protoLen);
 
     //申请内存地址
     auto proto_addr = (size_t *)malloc(protoLen);
@@ -31,8 +30,6 @@ size_t* ProtoBufMsg::encodeProtoStruct(const char* json_buffer) {
 
     *proto_addr = os->htonll(protoLen);
     proto_addr++;
-
-    printf("body len:%ld\n",body_len);
 
     auto dataStruct = (protoHeader*)proto_addr;
     dataStruct->version = SENTRY_VERSION;
